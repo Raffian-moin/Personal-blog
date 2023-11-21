bc4ee4ee705346a425363512a7f8038c03d41aed
Raffian-moin <raffianmoin69@gmail.com>
2023-11-21 23:12:07 +0600

    updated status column in migrations

--- database/migrations/2023_08_29_085647_create_tags_table.php
+++ database/migrations/2023_08_29_085647_create_tags_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
-            $table->boolean('status');
+            $table->boolean('status')->default(1);
             $table->unsignedBigInteger('created_by');
             $table->unsignedBigInteger('updated_by');
             $table->softDeletes('deleted_at');
