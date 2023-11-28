838abd16f0fca37c56c57e6be79dd118f0a57b10
Raffian-moin <raffianmoin69@gmail.com>
2023-11-28 22:58:59 +0600

    removed copy alert and changed button text after copying

--- public/admin/js/codeblockCopyButton.js
+++ public/admin/js/codeblockCopyButton.js
@@ -1,24 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll("pre").forEach((codeBlock) => {
+    document.querySelectorAll("pre").forEach((codeBlock, index) => {
         const copyButton = document.createElement("span");
+        copyButton.id = `copy_button-${index}`;
         copyButton.className = "copy-btn";
         copyButton.textContent = "Copy";
 
-        copyButton.onclick = function () {
+        copyButton.onclick = function (event) {
+            const clickedButtonId = event.target.id;
             const codeText = codeBlock.innerText;
-            copyTextToClipboard(codeText);
+            copyTextToClipboard(codeText, clickedButtonId);
         };
 
         codeBlock.insertBefore(copyButton, codeBlock.firstChild);
     });
 });
 
-function copyTextToClipboard(text) {
+function copyTextToClipboard(codeText, clickedButtonId) {
+    // Check if the codeText contains the "Copy" message
+    const copiedMessage = "Copy";
+    const isCopied = codeText.includes(copiedMessage);
+
+    // If the "Copied" message is found, exclude it from the copied codeText
+    const textToCopy = isCopied ? codeText.replace(copiedMessage, "") : codeText;
+
     if (navigator.clipboard && navigator.clipboard.writeText) {
         navigator.clipboard
-            .writeText(text)
+            .writeText(textToCopy)
             .then(() => {
-                alert("Code copied to clipboard!");
+                changeTextToCopied(clickedButtonId);
             })
             .catch((err) => {
                 console.error("Failed to copy: ", err);
@@ -26,11 +35,19 @@ function copyTextToClipboard(text) {
     } else {
         // Fallback for browsers not supporting clipboard API
         const textArea = document.createElement("textarea");
-        textArea.value = text;
+        textArea.value = textToCopy;
         document.body.appendChild(textArea);
         textArea.select();
         document.execCommand("copy");
         document.body.removeChild(textArea);
-        alert("Code copied to clipboard (fallback method)!");
+        changeTextToCopied(clickedButtonId);
     }
+}
+
+function changeTextToCopied(clickedButtonId) {
+    let clickedCopyButton = document.getElementById(clickedButtonId);
+    clickedCopyButton.innerHTML = 'Copied';
+    setTimeout(() => {
+        clickedCopyButton.innerHTML = "Copy";
+    }, 1000);
 }
